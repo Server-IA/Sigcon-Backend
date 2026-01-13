676d6680679a28ffc1f7751620941c3e7bf1ea3a
NicolasPerezU <u20221204357@usco.edu.co>
2026-01-13 18:08:34 -0500

    register,login,forgot-password,logout

---
+++ src/main/java/com/sigcon/backend/users/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sigcon.backend.users.domain.repository;
+
+import com.sigcon.backend.users.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    List<User> findAllByRoles_Name(String roleName);
+}
