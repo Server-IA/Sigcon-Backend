864b3e437ade51d555e7c4e8abe53d4b1a37b5bf
WilliamsBD8 <66226923+WilliamsBD8@users.noreply.github.com>
2026-01-21 22:26:51 -0500

    req #2 listas contables (#14)
    Co-authored-by: NicolasPerezU <u20221204357@usco.edu.co>

--- src/main/java/com/sigcon/backend/accounting_lists/application/ChartOfAccountDTO.java
+++ src/main/java/com/sigcon/backend/accounting_lists/application/ChartOfAccountDTO.java
@@ -3,8 +3,10 @@ package com.sigcon.backend.accounting_lists.application;
 import com.sigcon.backend.accounting_lists.domain.model.enums.AccountClass;
 import com.sigcon.backend.accounting_lists.domain.model.enums.AccountLevel;
 import com.sigcon.backend.accounting_lists.domain.model.enums.AccountNature;
+import com.sigcon.backend.accounting_lists.domain.model.enums.AccountStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,21 +19,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ChartOfAccountDTO {
 
-    @NotBlank(message = "El código oficial de la cuenta es obligatorio")
-    @Size(min = 1, max = 10, message = "El código oficial debe tener entre 1 y 10 caracteres")
+    @Pattern(regexp = "^[\\w\\-\\s]{1,100}$", message = "Por favor siga el formato de los filtros")
     private String code;
 
-    @NotBlank(message = "El nombre de la cuenta es obligatorio")
-    @Size(min = 1, max = 100, message = "El nombre de la cuenta debe tener máximo 100 caracteres")
+    @Pattern(regexp = "^[\\w\\-\\s]{1,100}$", message = "Por favor siga el formato de los filtros")
     private String name;
 
-    @NotNull(message = "La clase de la cuenta es obligatoria")
     private AccountClass accountClass;
 
-    @NotNull(message = "El nivel jerárquico de la cuenta es obligatorio")
     private AccountLevel level;
 
-    @NotNull(message = "La naturaleza de la cuenta es obligatoria")
     private AccountNature nature;
+
+    private AccountStatus status;
 
 }
--- src/main/java/com/sigcon/backend/accounting_lists/interfaces/ChartOfAccountController.java
+++ src/main/java/com/sigcon/backend/accounting_lists/interfaces/ChartOfAccountController.java
@@ -1,16 +1,16 @@
 package com.sigcon.backend.accounting_lists.interfaces;
 
 import com.sigcon.backend.accounting_lists.application.ChartOfAccountDTO;
+import com.sigcon.backend.accounting_lists.domain.model.ChartOfAccount;
 import com.sigcon.backend.accounting_lists.domain.service.ChartOfAccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chartOfAccounts")
@@ -33,6 +33,24 @@ public class ChartOfAccountController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error al guardar la información, intente nuevamente");
+        }
+    }
+
+    @GetMapping
+    @PreAuthorize( "hasAuthority('PERM_VIEW_CHART_OF_ACCOUNT')")
+    public ResponseEntity<?> searchChartOfAccounts(@ModelAttribute ChartOfAccountDTO request, Pageable pageable) {
+        try {
+            Page<ChartOfAccount> result =
+                    chartOfAccountService.searchChartOfAccounts(request, pageable);
+
+            return ResponseEntity.ok(result);
+
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            return ResponseEntity.ok(e.getMessage());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al consultar datos, intente nuevamente");
         }
     }
 
