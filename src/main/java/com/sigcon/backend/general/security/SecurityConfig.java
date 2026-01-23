24597771c3286112e3326a5a9e8ed613a2dd2f5d
WilliamsBD8 <66226923+WilliamsBD8@users.noreply.github.com>
2026-01-23 16:02:08 -0500

    Features/nicolas (#21)
    * req #2 listas contables
    
    * cors
    
    ---------
    
    Co-authored-by: NicolasPerezU <u20221204357@usco.edu.co>

--- src/main/java/com/sigcon/backend/general/security/SecurityConfig.java
+++ src/main/java/com/sigcon/backend/general/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -93,6 +94,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize ->
