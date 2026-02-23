f4323dc183e2c0ee431bc85917a7ad8e47491800
WilliamsBD8 <wsbonilladiaz@gmail.com>
2026-02-23 16:21:36 -0500

    Configuracion tokens para swagger

--- src/main/java/com/sigcon/backend/general/config/OpenApiConfig.java
+++ src/main/java/com/sigcon/backend/general/config/OpenApiConfig.java
@@ -1,5 +1,9 @@
 package com.sigcon.backend.general.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -9,6 +13,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @io.swagger.v3.oas.annotations.info.Info(title = "SIGCON API", version = "1.0"),
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 public class OpenApiConfig {
 
     @Bean
