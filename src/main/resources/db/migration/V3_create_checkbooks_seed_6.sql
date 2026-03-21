375eedf99c31658867bcdf53967ed85f011f63a4
WilliamsBD8 <wsbonilladiaz@gmail.com>
2026-03-20 23:04:15 -0500

    Ajuste checkbooks

--- src/main/resources/db/migration/V3_create_checkbooks_seed_6.sql
+++ src/main/resources/db/migration/V3_create_checkbooks_seed_6.sql
@@ -12,5 +12,11 @@ INSERT INTO checkbooks (
     created_at,
     updated_at
 )
-VALUES
-(1, 'CHK-001', 'Bancolombia', 1000, 1100, 101, 0, 101, CURRENT_DATE, 'ACTIVA', NOW(), NOW());
+SELECT *
+FROM (
+    VALUES
+    (1, 'CHK-001', 'Bancolombia', 1000, 1100, 101, 0, 101, CURRENT_DATE, 'ACTIVA', NOW(), NOW())
+) AS v(bank_account_id, checkbook_number, issuing_bank, check_start_number, check_end_number, total_checks, used_checks, available_checks, received_date, status, created_at, updated_at)
+WHERE NOT EXISTS (
+    SELECT 1 FROM checkbooks WHERE checkbook_number = v.checkbook_number
+);
